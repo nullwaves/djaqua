481d433e958cbccbb52fe1a48d674fe8fd16ee61
null <evanb96@gmail.com>
2023-05-09 13:24:28 -0500

    Update views.py

--- tanky/views.py
+++ tanky/views.py
@@ -1,11 +1,8 @@
-from typing import Any, Dict
-from django.forms.models import BaseModelForm
-from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
 
 from .models import Inhabitant, Tank, Spawn, WaterTest
 
